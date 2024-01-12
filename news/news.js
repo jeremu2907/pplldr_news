178ecbc70fdf9705c82370505c695650cb8019c9
jeremu2907 <jeremu2907@gmail.com>
2024-01-11 22:22:50 -0600

    new article

--- news/news.js
+++ news/news.js
@@ -2,6 +2,13 @@ const newsListDiv = document.getElementById('news-list');
 
 const newsListInfo = [
     {
+        date: "January 11th, 2023",
+        title: "Navigating the Corporate Landscape:",
+        titleExt: "Overcoming Challenges in Military-to-Civilian Transition",
+        subtitle: "Vol. I, Art. II",
+        url: "jan112024.html",
+    },
+    {
         date: "January 2nd, 2023",
         title: "Revolutionizing How Top Talent Finds their Quality People Leader",
         subtitle: "Vol. I, Art. I",
@@ -19,6 +26,11 @@ for (const newsInfo of newsListInfo) {
     headingElement.textContent = newsInfo.title;
     anchorElement.appendChild(headingElement);
 
+    const headingExtElement = document.createElement('h5');
+    if (newsInfo.titleExt) {
+        headingExtElement.textContent = newsInfo.titleExt;
+    }
+
     const dateParagraph = document.createElement('p');
     dateParagraph.textContent = newsInfo.date;
 
@@ -26,8 +38,10 @@ for (const newsInfo of newsListInfo) {
     subtitleParagraph.textContent = newsInfo.subtitle;
 
     newsItemDiv.appendChild(anchorElement);
+    if (newsInfo.titleExt) {
+        newsItemDiv.appendChild(headingExtElement);
+    }
     newsItemDiv.appendChild(dateParagraph);
     newsItemDiv.appendChild(subtitleParagraph);
-
     newsListDiv.appendChild(newsItemDiv);
 }
