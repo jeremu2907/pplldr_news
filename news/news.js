64389582ca8cb64ea791b124371838fc2612cbba
jeremu2907 <jeremu2907@gmail.com>
2024-01-11 22:47:08 -0600

    remove art vol

--- news/news.js
+++ news/news.js
@@ -5,13 +5,11 @@ const newsListInfo = [
         date: "January 11th, 2023",
         title: "Navigating the Corporate Landscape:",
         titleExt: "Overcoming Challenges in Military-to-Civilian Transition",
-        subtitle: "Vol. I, Art. II",
         url: "jan112024.html",
     },
     {
         date: "January 2nd, 2023",
         title: "Revolutionizing How Top Talent Finds their Quality People Leader",
-        subtitle: "Vol. I, Art. I",
         url: "jan22024.html",
     },
 ];
@@ -34,14 +32,10 @@ for (const newsInfo of newsListInfo) {
     const dateParagraph = document.createElement('p');
     dateParagraph.textContent = newsInfo.date;
 
-    const subtitleParagraph = document.createElement('p');
-    subtitleParagraph.textContent = newsInfo.subtitle;
-
     newsItemDiv.appendChild(anchorElement);
     if (newsInfo.titleExt) {
         newsItemDiv.appendChild(headingExtElement);
     }
     newsItemDiv.appendChild(dateParagraph);
-    newsItemDiv.appendChild(subtitleParagraph);
     newsListDiv.appendChild(newsItemDiv);
 }
